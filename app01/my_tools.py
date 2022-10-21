1ef386624a6824a282170af5105d7aa70917674c
JiyuHe <he_jiyu@163.com>
2022-10-21 22:52:54 +0800

    modified:   app01/my_tools.py 	modified:   app01/static/img/image.jpg 	modified:   app01/views.py

--- app01/my_tools.py
+++ app01/my_tools.py
@@ -2,6 +2,7 @@ import base64
 import os
 import re
 from pathlib import Path
+from select import select
 from django.db import connection
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,10 +41,34 @@ def save_photo(photo):
     d = re.sub(r'data:image/jpg;base64,', "", d)
     d = re.sub(r'data:image/jpeg;base64,', "", d)
     # print(d)
-    # photo = base64.b64decode(d)
+    photo = base64.b64decode(d)
     filename = os.path.join(BASE_DIR, 'app01/static/img/')
     filename = filename + 'image.jpg'  # I assume you have a way of picking unique filenames
     print(filename)
     with open(filename, 'wb') as f:
         f.write(photo)
     return filename
+
+
+def index_search_goods(info):
+    sql = "select * from goods_view where goods_name like '%{0}%'".format(info)
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    print("rows:",rows)
+    list = []
+    for row in rows:
+        dic = {
+        "shop_name": row[5],
+        "shop_num": "",
+        "goods_num": row[0],
+        "goods_name": row[1],
+        "inventory_number": row[6],
+        "goods_price": row[3],
+        'goods_description': row[2],
+        'shopper_description': '',#商家描述
+        'goods_photo': row[4]
+        }
+        print("dic",dic)
+        list.append(dic)
+    return list
+        # dic['shop_name']=row[1]
