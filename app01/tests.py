99c479e3c6af6649956d1a2a481e3f45c5b2cc6d
JiyuHe <he_jiyu@163.com>
2022-10-22 01:46:05 +0800

    pyc

--- app01/tests.py
+++ app01/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+import random
 # Create your tests here.
+digits='0123456789'
+ascii_letters='s'
+str_list =[random.choice(digits + ascii_letters) for i in range(10)]
+random_str ='qqq'.join(str_list)
+
+goods_num = 'goods'+''.join([random.choice('0123456789') for i in range(5)])
+
+print(goods_num)
