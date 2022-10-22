ea4f754b9a007c15e0b41934f9875c7397730b59
kaoxing <3032738029@qq.com>
2022-10-22 13:06:48 +0800

    modified:   app01/my_tools.py

--- app01/my_tools.py
+++ app01/my_tools.py
@@ -1,5 +1,8 @@
 import base64
 import os
+from pydoc import describe
+import random
+
 import re
 from pathlib import Path
 from select import select
@@ -12,7 +15,7 @@ cursor = connection.cursor()
 
 def shopper_exist(id, pwd):
     # 用户登录
-    sql = "select * from shopper_table where shopper_num=" + id
+    sql = "select * from shopper_table where shopper_num='{}'".format(id)
     cursor.execute(sql)
     rows = cursor.fetchall()
     print(id,pwd,rows)
@@ -23,15 +26,13 @@ def shopper_exist(id, pwd):
 
 def shop_exist(id, pwd):
     # 商家登录
-    sql = "select * from shop_table where shop_num=" + id
+    sql = "select * from shop_table where shop_num='{}'".format(id)
+    print(sql)
     cursor.execute(sql)
     rows = cursor.fetchall()
     if len(rows) != 0 and rows[0][2] == pwd:
         return rows[0][1]
     return None
-
-
-# def
 
 
 def save_photo(photo):
@@ -54,7 +55,7 @@ def index_search_goods(info):
     sql = "select * from goods_view where goods_name like '%{0}%'".format(info)
     cursor.execute(sql)
     rows = cursor.fetchall()
-    print("rows:",rows)
+    # print("rows:",rows)
     list = []
     for row in rows:
         dic = {
@@ -70,5 +71,52 @@ def index_search_goods(info):
         }
         print("dic",dic)
         list.append(dic)
+        print()
     return list
-        # dic['shop_name']=row[1]
+
+
+def mgood_get(id):
+    sql = "select * from shop_goods_view where shop_num = '{0}'".format(id)
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    # print("rows:",rows)
+    list = []
+    for row in rows:
+        dic = {
+        "goods_num": row[1],
+        "goods_name": row[2],
+        "goods_number": row[6],
+        "goods_price": row[4],
+        'goods_description': row[3],
+        # 'order_address': '123',
+        # 'statu': '',
+        'goods_photo': row[5]
+        }
+        # print("dic",dic)
+        list.append(dic)
+    return list
+
+
+def mgood_post(data):
+    # print("!!!!!!!!!!!!!!!!!!!!!",data.get("id"))
+    # print("!!!!!!!!!!!!!!!!!!!!!",data.keys())
+    name = data.get("goods_name")
+    description = data.get("goods_description")
+    price = data.get("goods_price")
+    picture = ""
+    amount = data.get("goods_number")
+    goods_num = ""
+    shop_num = data.get("id")
+    ope=''
+    # ope='上架'
+    if ope == '上架':
+        goods_num = 'goods'+''.join([random.choice('0123456789') for i in range(5)]) #随机生成商品号 
+        sql = "insert into goods_table values ('{0}','{1}','{2}',{3},'{4}')".format(goods_num,name,description,price,picture)
+        cursor.execute(sql)
+        sql = "insert into inventory_table values('{0}','{1}',{2},{3})".format(shop_num,goods_num,amount,0)
+        cursor.execute(sql)
+    elif ope == '修改':
+        sql = "updata goods_table set goods_name = '{0}',goods_description = '{1}',goods_price= '{2}',goods_picture= '{3}' where goods_num = '{4}'".format(name,description,price,picture,goods_num)
+        cursor.execute(sql)
+        sql = "updata inventory_table set inventory_amount = {0} where goods_num = '{1}' and shop_num = '{2}'".format(amount,goods_num,shop_num)
+        cursor.execute(sql)
